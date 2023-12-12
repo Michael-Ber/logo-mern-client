07060a38a1e23d5524fd5b880917bf517fd34355
Бершадский Михаил Сергеевич <Michail.Bershadsky@ilimtimber.corp>
2023-12-12 10:25:58 +0800

    change to hash router

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './components/app/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { hashRouter as Router } from 'react-router-dom';
 
 import { store } from './redux/store';
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={ store }> 
+    <Provider store={store}>
         <Router>
             <App />
         </Router>
